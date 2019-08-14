18340417bf55f89b2ff6592d41456af50577a2b5
Aleksandr Krokhmalnyi <magr0s.ak@gmail.com>
2019-08-14 13:01:44 +0300

    new example

--- src/App.vue
+++ src/App.vue
@@ -1,189 +1,215 @@
-<template>
-  <div id="app" class="app">
-    <h1 class="app-title">Vue Scrollmagic plugin!</h1>
-    <div id="trigger"></div>
-    <div class="info">
-      <span class="info-item">Size: {{ info.size }}</span>
-      <span class="info-item">Is vertical scrolling: {{ info.vertical }}</span>
-      <span class="info-item">Current scroll position: {{ info.scrollPos }}</span>
-      <span class="info-item">Last scroll direction: {{ info.scrollDirection }}</span>
-      <span class="info-item">Container: {{ this.scrollContainer }}</span>
-      <span class="info-item">Is element: {{ info.isDocument }}</span>
-    </div>
-    <div id="trigger2"></div>
-    <div id="animate2">Animated</div>
-    <div class="app-footer">
-      <a href="#" @click.prevent="$scrollmagic.scrollTo(0)">Back to top</a>
-    </div>
-  </div>
-</template>
-
 <script>
-import { TweenMax, Elastic } from 'gsap'
+import { TweenMax, TimelineMax } from 'gsap'
 import 'ScrollToPlugin'
 
 export default {
-  data () {
-    return {
-      info: {
-        size: this.$scrollmagic.info('size'),
-        vertical: this.$scrollmagic.info('vertical'),
-        scrollPos: this.$scrollmagic.info('scrollPos'),
-        scrollDirection: this.$scrollmagic.info('scrollDirection'),
-        container: this.$scrollmagic.info('container'),
-        isDocument: this.$scrollmagic.info('isDocument')
-      }
-    }
-  },
-
-  computed: {
-    scrollContainer () {
-      return (this.info.container === window) ? 'window' : this.info.container.tagName
-    }
-  },
-
-  created () {
-    this.$scrollmagic.handleScrollTo = function (target) {
-      TweenMax.to(window, 1.5, {
-        scrollTo: {
-          y: target,
-          autoKill: false
-        }
-      })
-    }
-
-    window.addEventListener('scroll', this.handleScroll)
-  },
-
   mounted () {
-    const scene = this.$scrollmagic.scene({
-      triggerElement: '#trigger',
-      triggerHook: 0,
-      duration: '50%',
-      reverse: true
+    const wrapScene = this.$scrollmagic.scene({
+      triggerElement: '#app',
+      triggerHook: 0.15,
+      duration: '50%'
     })
+      .setTween(
+        TweenMax.from('#wrapper', 1, {
+          css: {
+            left: '-100%',
+            overflowY: 'hidden'
+          }
+        })
+      )
+      .addIndicators()
 
-    this.$scrollmagic.addScene(
-      scene
-        .setTween(
-          TweenMax.from('.info', 20, {
-            autoAlpha: 0,
-            yPercent: 50,
-            ease: Elastic.easeInOut
-          })
-        )
-        .addIndicators()
-    )
-
-    // Declare Scene
-    const scene2 = this.$scrollmagic.scene({
-      // ID of element where animation starts
-      triggerElement: '#trigger2',
-
-      // {0,0.5,1} - animations starts from {top,center,end} of window
-      triggerHook: 0.5,
-
-      // Duration of animation
-      duration: 300
-    })
-      // Declaration of animation and attaching to element
-      .setTween('#animate2', {
+    const timeline = new TimelineMax()
+    timeline.add([
+      TweenMax.to('#shape1', 1, {
         css: {
-          borderTop: '30px solid white',
-          backgroundColor: 'blue',
-          scale: 0.7 // the tween durtion can be omitted and defaults to 1
+          top: '95%',
+          marginTop: '-50px',
+          borderRadius: '50%',
+          scale: '0.5'
+        }
+      }),
+
+      TweenMax.fromTo('#shape2', 1,
+        {
+          scale: '0.2'
+        },
+        {
+          scale: '1.8',
+          borderWidth: 0
+        }
+      ),
+
+      TweenMax.to('#shape3', 1, {
+        css: {
+          bottom: '95%',
+          marginBottom: '-50px',
+          borderRadius: '50%',
+          scale: '0.5'
         }
       })
-      // Helpful tags for orientation on the screen
-      .addIndicators({ name: '2 (duration: 300)' })
+    ])
 
-    // Add Scene to controller
-    this.$scrollmagic.addScene(scene2)
+    const shapeScene = this.$scrollmagic.scene({
+      triggerElement: '#app',
+      triggerHook: 0,
+      duration: '100%'
+    })
+      .setTween(timeline)
+      .addIndicators()
+
+    // add scenes to window scrollmagic controller
+    this.$scrollmagic.addScene([wrapScene, shapeScene])
+
+    const timeline2 = new TimelineMax()
+
+    timeline2.add([
+      TweenMax.to('#wrapper', 1, {
+        css: {
+          boxShadow: '0 8px 9px -5px rgba(0,0,0,0.2), 0 15px 22px 2px rgba(0,0,0,0.14), 0 6px 28px 5px rgba(0,0,0,0.12)'
+        }
+      }),
+
+      TweenMax.to('.container', 1, {
+        css: {
+          opacity: 0.1
+        }
+      })
+    ])
+
+    const wrapScene2 = this.$scrollmagic.scene({
+      triggerElement: '#wrapper-inner',
+      triggerHook: 0,
+      duration: '100%'
+    })
+      .setTween(timeline2)
+      .addIndicators()
+
+    // attach scrollmagic controller to element
+    this.$scrollmagic.attachTo(this.$refs.wrapBox)
+
+    // add scene to element scrollmagic controller
+    this.$refs.wrapBox
+      .$scrollmagic.addScene(wrapScene2)
   },
 
-  destroyed () {
-    window.removeEventListener('scroll', this.handleScroll)
-  },
+  render (h) {
+    return h(
+      'div',
+      {
+        attrs: {
+          id: 'app'
+        }
+      },
+      [
+        h(
+          'div',
+          {
+            class: 'container'
+          },
+          [
+            h('div', { class: 'shape', attrs: { id: 'shape1' } }),
+            h('div', { class: 'shape', attrs: { id: 'shape2' } }),
+            h('div', { class: 'shape', attrs: { id: 'shape3' } })
+          ]
+        ),
 
-  methods: {
-    updateInfo () {
-      this.info = this.$scrollmagic.info()
-    },
+        h(
+          'div',
+          {
+            attrs: {
+              id: 'wrapper'
+            },
 
-    handleScroll (evt) {
-      this.updateInfo()
-    }
+            ref: 'wrapBox'
+          },
+          [
+            h(
+              'div',
+              {
+                attrs: {
+                  id: 'wrapper-inner'
+                }
+              },
+              [
+                h(
+                  'h2',
+                  {
+                    style: {
+                      marginLeft: '25px'
+                    }
+                  },
+                  'Scroll me!!!'
+                )
+              ]
+            )
+          ]
+        )
+      ]
+    )
   }
 }
 </script>
 
 <style lang="scss">
-html {
-  font-size: 15px;
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-}
 body {
-  background-color: #121212;
-  color: white;
+  margin: 0
 }
 
-.app {
-  min-height: 250vh;
-
-  &-title {
-    margin-top: 60px;
-    text-align:center;
-  }
-
-  #animate2 {
-    border: 1px solid white;
-    background-color: grey;
-    padding: 10px;
-  }
-
-  &-footer {
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    padding: 5px 0;
-    text-align: center;
-
-    a{
-      color: white;
-      transition: color .2s ease-in-out;
-
-      &:hover,
-      &:active {
-        color: rgba(white, .75);
-      }
-    }
-  }
+#app {
+  height: 200vh;
+  display: flex;
+  background-color: #f5f5f5;
 }
 
-.info {
+.container {
+  width: 100%;
+  height: 100%;
+  background-color: #027be3;
+}
+
+#wrapper {
   position: fixed;
-  top: 150px;
-  left: 0;
-  right: 0;
-  width: 250px;
-  margin: 0 auto;
-  padding: 1rem 2rem;
-  border-radius: 5px;
-  background: rgba(#000, .5);
-  text-align: center;
-  transition: background .3s ease-in-out;
-
-  &-item {
-    display: block;
-  }
-
-  &.red-bg {
-    background: rgba(red, .5);
-  }
+  top: 25vh;
+  left: 10vw;
+  width: 55vw;
+  height: 50vh;
+  background-color: #e0e0e0;
+  z-index: 10;
+  overflow-y: scroll;
 }
 
-#trigger {
+#wrapper-inner {
+  width: 100%;
   height: 100vh;
+}
+
+.shape {
+  position: fixed;
+  width: 100px;
+  height: 100px;
+  opacity: .8;
+}
+
+#shape1 {
+  top: 5%;
+  left: 50%;
+  background-color: #d84315;
+}
+
+#shape2 {
+  left: 75%;
+  top: 50%;
+  margin-top: -50px;
+  margin-left: -50px;
+  background-color: #cddc39;
+  border-radius: 50%;
+  border: 50px solid #21ba45;
+}
+
+#shape3 {
+  right: 5%;
+  bottom: 5%;
+  background-color: #006064;
 }
 </style>
